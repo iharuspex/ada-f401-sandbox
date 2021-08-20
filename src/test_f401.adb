5cc623a8ca9b2186737b242596aac01d8ad42132
aparaev <nabrosok-hd@yandex.ru>
2021-08-20 19:58:20 +0700

    wip

--- src/test_f401.adb
+++ src/test_f401.adb
@@ -1,6 +1,18 @@
-with Ada.Real_Time;
+with Ada.Text_IO;
+with STM32F401.RCC;
+
 
 procedure Test_F401 is 
+   
+   procedure Init_Periph is
+     
+   begin
+      
+      loop
+         null;
+      end loop;
+   end Init_Periph;
+   
 begin
    Ada.Text_IO.Put_Line("Hello world!");
    null;
